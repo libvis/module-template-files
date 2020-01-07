5aa34cff87a21f01c4d227ad8e13d8315ac27cdc
Danil Lykov <lkv97dn@gmail.com>
2020-01-07 04:49:16 -0600

    updated templates to working

---
+++ {{cookiecutter.name}}/readme.md
@@ -0,0 +1,5 @@
+This is a {{cookiecutter.name}} [webvis](http://webvis.dev) module
+
+# Installation
+
+`webvis_mods install {{cookiecutter.name}} {{cookiecutter.name}}-back.py {{cookiecutter.name}}-front.coffee`
---
+++ {{cookiecutter.name}}/{{cookiecutter.name}}_back.py
@@ -0,0 +1,11 @@
+from webvis.modules import BaseModule
+import json
+
+class {{cookiecutter.name}}(BaseModule):
+    name="{{cookiecutter.name}}"
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.hello = 'world'
+
+    def serial(self):
+        return json.dumps(self)
